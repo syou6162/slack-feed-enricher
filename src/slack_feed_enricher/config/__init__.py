a2ecf7a08d0061d3490349258cb03d834861519b
Yasuhisa Yoshida <syou6162@gmail.com>
2026-01-22 03:37:06 +0900

    feat: configモジュールの統合エクスポートを追加
    config/__init__.pyでEnvConfig、AppConfig、load_env_config、
    load_app_configを統合エクスポート。
    
    これによりユーザーは`from slack_feed_enricher.config import EnvConfig`
    のように直接インポートできるようになる。

--- src/slack_feed_enricher/config/__init__.py
+++ src/slack_feed_enricher/config/__init__.py
@@ -1,1 +1,11 @@
 """設定管理モジュール"""
+
+from slack_feed_enricher.config.app import AppConfig, load_app_config
+from slack_feed_enricher.config.env import EnvConfig, load_env_config
+
+__all__ = [
+    "AppConfig",
+    "EnvConfig",
+    "load_app_config",
+    "load_env_config",
+]
